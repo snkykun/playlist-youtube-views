d742f2717c01c07e7807b8d6a3a7a615371e50a3
Kyy <68399818+snkykun@users.noreply.github.com>
2022-04-09 16:46:28 -0400

    first commit

---
+++ botkeys_EXAMPLE.py
@@ -0,0 +1,4 @@
+#rename to botkeys.py and set your keys in the quotes
+
+discord_key = ''
+yt_api_key = ''
---
+++ viewCount.py
@@ -0,0 +1,51 @@
+import schedule
+import time
+import botkeys
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from googleapiclient.discovery import build
+
+youtube = build('youtube', 'v3', developerKey=botkeys.yt_api_key)
+
+def kyyViewCount():
+
+    kyy_viewCounts = 0
+    kyy_nextPageToken = None
+    print("working")
+    while True:
+
+        # kyy_viewCounts
+        kyy_request = youtube.playlistItems().list(
+                part='contentDetails',
+                playlistId='PL5XH1T5EYkawLWSWS4H0WzBE6pn6M1QTY',
+                maxResults=50,
+                pageToken=kyy_nextPageToken
+
+            )
+        kyy_response = kyy_request.execute()
+        kyy_nextPageToken = kyy_response.get('nextPageToken')
+        for items in kyy_response['items']:
+            try:
+                kyy_request = youtube.videos().list(
+                    part='statistics',
+                    id=items['contentDetails']['videoId']
+                )
+                kyy_response = kyy_request.execute()
+                kyy_viewCounts = kyy_viewCounts + int(kyy_response['items'][0]['statistics']['viewCount'])
+            except:
+                None
+
+        if not kyy_nextPageToken:
+            break
+    with open('/var/www/html/wp-content/plugins/python-code/views.txt', 'w') as text_file:
+
+        text_file.write(str(kyy_viewCounts))
+
+
+
+schedule.every(1).hour.do(kyyViewCount)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
